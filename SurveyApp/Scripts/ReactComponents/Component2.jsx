f9cfe5171fd1ddb0b820023e1b7cbabc459e369f
Chris <clc523@gmail.com>
2018-02-18 12:19:39 -0700

    react .net integration testing. remembering react.

---
+++ SurveyApp/Scripts/ReactComponents/Component2.jsx
@@ -0,0 +1,9 @@
+﻿class Component2 extends React.Component {
+    render() {
+        return (
+            <div>I react component 2!</div>
+        );
+    }
+}
+
+export default Component2;
--- SurveyApp/Scripts/ReactTest.jsx
+++ SurveyApp/Scripts/ReactTest.jsx
@@ -1,11 +1,22 @@
-﻿var TestComponent = React.createClass({
-    render: function () {
+﻿import Component2 from "./ReactComponents/Component2.jsx";
+
+class TestComponent extends React.Component {
+    testClassMethod() {
+        return 10 + 2;
+    }   
+
+    render() {
         return (
             <h1 className="commentBox">
-                Hello, world! I am a REACT Component.
+                Hello, world! I am a REACT Component! This is a test class method {this.testClassMethod()}!
             </h1>
         );
     }
-});
+}
 
-ReactDOM.render(<TestComponent />, document.getElementById("reactMount"));
+ReactDOM.render(
+    <div>
+        <Component2 />
+        <TestComponent />
+    </div>
+    , document.getElementById("reactMount"));
