4b0a13c9f226b4a63e2fc74af929f41d13e51a0f
Chris <clc523@gmail.com>
2018-02-19 09:51:42 -0700

    added more seed data for testing db queries

--- SurveyAppClassLibrary/Data/SeedData.cs
+++ SurveyAppClassLibrary/Data/SeedData.cs
@@ -17,9 +17,10 @@ namespace SurveyAppClassLibrary.Data
                 CreationDate = DateTime.Today
             };
 
-            Survey Survey1 = new Survey()
+            User TestUser2 = new User()
             {
-                Creator = TestUser1,
+                Username = "testuser2",
+                Email = "test2@test.com",
                 CreationDate = DateTime.Today
             };
 
@@ -36,10 +37,95 @@ namespace SurveyAppClassLibrary.Data
                 QuestionText = "How would you rate our product?"
             };
 
+            Question Q3 = new Question()
+            {
+                Creator = TestUser2,
+                CreationDate = DateTime.Today,
+                QuestionText = "How did you hear about us?"
+            };
+            Question Q4 = new Question()
+            {
+                Creator = TestUser2,
+                CreationDate = DateTime.Today,
+                QuestionText = "How would you rate your customer service experience?"
+            };
+            Question Q5 = new Question()
+            {
+                Creator = TestUser2,
+                CreationDate = DateTime.Today,
+                QuestionText = "Would you recommend our product to friend?"
+            };
+            Question Q6 = new Question()
+            {
+                Creator = TestUser2,
+                CreationDate = DateTime.Today,
+                QuestionText = "How likely are you to use our product again?"
+            };
+
+            Survey Survey1 = new Survey()
+            {
+                Creator = TestUser1,
+                CreationDate = DateTime.Today,
+                Questions = new List<Question>() { Q1, Q2}
+            };
+            Survey Survey2 = new Survey()
+            {
+                Creator = TestUser2,
+                CreationDate = DateTime.Today,
+                Questions = new List<Question>() { Q3, Q4, Q5, Q6 }
+            };
+            Survey Survey3 = new Survey()
+            {
+                Creator = TestUser2,
+                CreationDate = DateTime.Today,
+                Questions = new List<Question>() { Q3, Q6 }
+            };
+
+            Client Client1 = new Client()
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "client1@test.com",
+                ZipCode = "84101",
+                Surveys = new List<Survey>() {},
+                Owner = TestUser2
+            };
+            Client Client2 = new Client()
+            {
+                FirstName = "Jane",
+                LastName = "Smith",
+                Email = "client2@test.com",
+                ZipCode = "84101",
+                Surveys = new List<Survey>() { },
+                Owner = TestUser2
+            };
+            Client Client3 = new Client()
+            {
+                FirstName = "Adam",
+                LastName = "Smith",
+                Email = "client3@test.com",
+                ZipCode = "84101",
+                Surveys = new List<Survey>() { },
+                Owner = TestUser2
+            };
+            Client Client4 = new Client()
+            {
+                FirstName = "John",
+                LastName = "Keynes",
+                Email = "client4@test.com",
+                ZipCode = "84101",
+                Surveys = new List<Survey>() { },
+                Owner = TestUser1
+            };
+            Survey1.Clients = new List<Client>() { Client4 };
+            Survey2.Clients = new List<Client>() { Client1, Client2, Client3 };
+            Survey3.Clients = new List<Client>() { Client2, Client1 };
+
             _context.Users.Add(TestUser1);
-            _context.Questions.Add(Q1);
-            _context.Questions.Add(Q2);
-            _context.Surveys.Add(Survey1);
+            _context.Users.Add(TestUser2);
+            _context.Questions.AddRange(new List<Question>() { Q1, Q2, Q3, Q4, Q5, Q6 });
+            _context.Surveys.AddRange(new List<Survey>() { Survey1, Survey2, Survey3 });
+            _context.Clients.AddRange(new List<Client>() { Client1, Client2, Client3, Client4 });
 
             _context.SaveChanges();
         }
