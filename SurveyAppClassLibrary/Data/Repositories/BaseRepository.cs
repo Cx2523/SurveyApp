0695d190ef1af27ec40033b630ca25eba2d9191a
Chris <clc523@gmail.com>
2018-03-20 21:57:53 -0600

    Adding base repository class

---
+++ SurveyAppClassLibrary/Data/Repositories/BaseRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyAppClassLibrary.Data.Repositories
+{
+    public abstract class BaseRepository<TEntity>
+    {
+        protected Context Context { get; private set; }
+        //base repository context constructor
+        public BaseRepository(Context context)
+        {
+            Context = context;
+        }
+        //get all in DB set
+        public abstract TEntity GetList(int id);
+        //Get(id) in DB set
+        public abstract IList<TEntity> Get();
+        //INsert(entity)
+
+        //Delete(id)
+
+        //Update(entity)
+    }
+}
